b61abc46899fe287771a7aef9ba32ad995d2b2eb
Amir <garam08@bk.ru>
2025-07-04 14:33:35 +0300

    Added design for product card and product's list

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -11,6 +11,7 @@ def product_detail(request, id, slug):
     cart_product_form = CartAddProductForm()
     return render(request, 'shop/product/detail.html', {'product': product,
                                                         'cart_product_form': cart_product_form})
+    
 
 def product_list(request, category_slug=None):
     category = None
